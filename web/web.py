8fa24ed13cd6ba3b0e3991a70acce4f5f8cf5fdd
Max <maxfyk@gmail.com>
2022-10-24 20:42:22 +0800

    Update web.py

--- web/web.py
+++ web/web.py
@@ -125,7 +125,7 @@ def create_code_post():
 @app.route('/image-decode-request', methods=['post'])
 def send_image_decode_request():
     """Forward request to api"""
-    resp = requests.post(f'{API_URL}/coji-code/decode', json=request.get_data())
+    resp = requests.post(f'{API_URL}/coji-code/decode', json=request.get_json())
     return Response(resp.content, resp.status_code)
 
 
