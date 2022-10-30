aa1fac09629af1550b1789167da8fa896390adb8
Max <maxfyk@gmail.com>
2022-10-30 13:02:19 +0800

    imprv maps

--- web/web.py
+++ web/web.py
@@ -65,7 +65,7 @@ def location_decode(location):
     resp = r.get(f'{API_URL}/coji-code/get-by-city/{location}')
     if resp.status_code == 200:
         codes = resp.json().get('data', None)
-        return render_template('location-decode.html', CODES=enumerate(codes.items()))
+        return render_template('location-decode.html', CODES=enumerate(codes.items()), LOCATION=location)
     return render_template('index.html', ERROR='Something went wrong')
 
 
